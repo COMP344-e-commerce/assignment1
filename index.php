0ae6f4f5b7447b8459154a56dfe98ec9c4a12725
darrenchang <darrenchang951@gmail.com>
2018-07-15 08:30:05 +0800

    php var and function some random examples

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <title>Darren</title>
+</head>
+<body>
+<?php
+function myTest($a) {
+    $b = 10;
+    return $b + $a;
+}
+$name = "Darren";
+//echo "My name is ". $name. "<br>";
+//echo "My name is ". $name. "<br>";
+//echo "My name is ". $name. "<br>";
+echo myTest(5);
+?>
+</body>
+</html>
