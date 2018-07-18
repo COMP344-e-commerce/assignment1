3da43a51cc61b83a26fd77a97b0196409db77248
darrenchang <darrenchang951@gmail.com>
2018-07-18 11:28:31 +0800

    changed encoding

--- install.sql
+++ install.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS `user`
 (
   `userID`           INT          NOT NULL AUTO_INCREMENT,
-  `userFirstName`    VARCHAR(45)  NOT NULL,
-  `userLastName`     VARCHAR(45)  NOT NULL,
+  `userFirstName`    VARCHAR(45)  CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `userLastName`     VARCHAR(45)  CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `email`            VARCHAR(60)  NOT NULL,
-  `country`          VARCHAR(30)  NOT NULL,
-  `state`            VARCHAR(30)  NOT NULL,
-  `city`             VARCHAR(30)  NOT NULL,
-  `address`          VARCHAR(100) NOT NULL,
+  `country`          VARCHAR(30)  CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `state`            VARCHAR(30)  CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `city`             VARCHAR(30)  CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `address`          VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `postCode`         CHAR(6)      NOT NULL,
   `creditCardNumber` VARCHAR(32)  NOT NULL,
   `creditCardExpiry` DATETIME     NOT NULL,
@@ -16,23 +16,11 @@ CREATE TABLE IF NOT EXISTS `user`
   UNIQUE (`email`)
 );
 
-CREATE TABLE IF NOT EXISTS `userSession`
-(
-  `userID`  INT          NOT NULL,
-  `session` VARCHAR(128) NOT NULL,
-  PRIMARY KEY (`userID`, `session`),
-  CONSTRAINT `fk_user_session_user` FOREIGN KEY (`userID`)
-  REFERENCES `user` (`userID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-);
-
 CREATE TABLE IF NOT EXISTS `author`
 (
   `authorID`           INT         NOT NULL AUTO_INCREMENT,
-  `authorFirstName`    VARCHAR(45) NOT NULL,
-  `authorMiddleName`   VARCHAR(45) NULL,
-  `authorLastName`     VARCHAR(45) NOT NULL,
+  `authorFirstName`    VARCHAR(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `authorLastName`     VARCHAR(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `authorEmailAddress` VARCHAR(60) NULL,
   PRIMARY KEY (`authorID`)
 );
@@ -40,8 +28,8 @@ CREATE TABLE IF NOT EXISTS `author`
 CREATE TABLE IF NOT EXISTS `publisher`
 (
   `publisherID`        CHAR(5)     NOT NULL,
-  `publisherName`      VARCHAR(45) NULL,
-  `publisherCity`      VARCHAR(30) NULL,
+  `publisherName`      VARCHAR(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `publisherCity`      VARCHAR(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `publisherContactNo` CHAR(14)    NULL,
   PRIMARY KEY (`publisherID`)
 );
@@ -49,15 +37,15 @@ CREATE TABLE IF NOT EXISTS `publisher`
 CREATE TABLE IF NOT EXISTS `unit`
 (
   `unitID`   INT NOT NULL AUTO_INCREMENT,
-  `unitName` VARCHAR(30),
+  `unitName` VARCHAR(30) NOT NULL,
   PRIMARY KEY (`unitID`)
 );
 
 CREATE TABLE IF NOT EXISTS `book`
 (
   `bookISBN`    VARCHAR(45)   NOT NULL,
-  `bookTitle`   VARCHAR(45)   NOT NULL,
-  `cover`       VARCHAR(45)   NULL,
+  `bookTitle`   VARCHAR(45)   CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `cover`       VARCHAR(45)   CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
   `bookPrice`   DECIMAL(6, 2) NOT NULL,
   `pageNumber`  INT           NOT NULL,
   `publisherID` CHAR(5)       NULL,
@@ -89,6 +77,21 @@ CREATE TABLE IF NOT EXISTS `wishList`
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_wish_list_book` FOREIGN KEY (`bookISBN`)
+  REFERENCES `book` (`bookISBN`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+
+CREATE TABLE IF NOT EXISTS `shopping_cart`
+(
+  `userID`   INT         NOT NULL,
+  `bookISBN` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`userId`, `bookISBN`),
+  CONSTRAINT `fk_shopping_cart_user` FOREIGN KEY (`userID`)
+  REFERENCES `user` (`userID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_shopping_cart_book` FOREIGN KEY (`bookISBN`)
   REFERENCES `book` (`bookISBN`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
